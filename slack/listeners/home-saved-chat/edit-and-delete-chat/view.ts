0f4a33c5f755dad0bfa1643891209f5befa48ca6
Yuncheng Yang <fabioyang781@gmail.com>
2023-07-23 23:24:53 +1000

    move edit and delete button to overflow;

---
+++ slack/listeners/home-saved-chat/edit-and-delete-chat/view.ts
@@ -0,0 +1,62 @@
+import { Overflow, View } from "@slack/bolt";
+import { editChatOverflowValue, deleteChatOverflowValue } from "./utils";
+
+export const deleteChatActionId = 'delete_saved_thread';
+export const deleteChatConfirmViewId = 'delete-chat-view';
+export const overflowActionId = 'edit-and-delete-action';
+
+export const deleteChatConfirmView = (external_id: string): View => ({
+    "type": "modal",
+    "callback_id": deleteChatConfirmViewId,
+    external_id,
+    "submit": {
+        "type": "plain_text",
+        "text": "Confirm",
+        "emoji": true
+    },
+    "close": {
+        "type": "plain_text",
+        "text": "Cancel",
+        "emoji": true
+    },
+    "title": {
+        "type": "plain_text",
+        "text": "Delete this chat :broom:",
+        "emoji": true
+    },
+    "blocks": [
+        {
+            "type": "section",
+            "text": {
+                "type": "plain_text",
+                "text": "Confirm you want to DELETE this entry from your saved chat",
+                "emoji": true
+            }
+        }
+    ]
+})
+
+
+export const chatOverflowAccesoryView = (threadId: string): Overflow => ({
+    "type": "overflow",
+    "action_id": overflowActionId,
+    "options": [
+        {
+            "text": {
+                "type": "plain_text",
+                "text": "Edit :pencil2:",
+                "emoji": true
+            },
+            "value": editChatOverflowValue(threadId)
+        },
+        {
+            "text": {
+                "type": "plain_text",
+                "text": "Delete :x:",
+                "emoji": true
+            },
+            "value": deleteChatOverflowValue(threadId)
+        },
+
+    ]
+})
