a08efae3f5a9a2fd18961abebffbd26b6faec0fc
Yuncheng <fabioyang781@gmail.com>
2023-10-26 23:45:51 +1100

    commented out the legacy app

--- netlify/functions/slack/slack.ts
+++ netlify/functions/slack/slack.ts
@@ -32,9 +32,10 @@ const app = new App({
   authorize: authorizeFn,
 });
 
-registerHomeTabListeners(app);
-registerSaveChatHandler(app);
-registerConfluenceHandlers(app);
+//Legacy App
+// registerHomeTabListeners(app);
+// registerSaveChatHandler(app);
+// registerConfluenceHandlers(app);
 
 const handler: Handler = async (event: HandlerEvent, context: HandlerContext) => {
   const payload = parseRequestBody(event.body, event.headers["content-type"]);
